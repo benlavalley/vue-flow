c4a81206af1e5e63a027a40cc27bdc21507d3715
nibbl LLC <ben@nibbl.com>
2025-07-24 10:21:05 -0400

    Add sizeMultiplier prop to controls for scaling
    Introduces a sizeMultiplier prop to ControlButton and Controls components, allowing dynamic scaling of button size, icon size, and padding. Updates styles and types to support this feature, enabling more flexible UI customization.

--- packages/controls/src/ControlButton.vue
+++ packages/controls/src/ControlButton.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+interface Props {
+  sizeMultiplier?: number
+}
+
+const { sizeMultiplier = 1 } = defineProps<Props>()
+</script>
+
 <script lang="ts">
 export default {
   name: 'ControlButton',
@@ -6,7 +14,16 @@ export default {
 </script>
 
 <template>
-  <button class="vue-flow__controls-button">
+  <button 
+    class="vue-flow__controls-button"
+    :style="{
+      padding: `${5 * sizeMultiplier}px`,
+      width: `${16 * sizeMultiplier}px`,
+      height: `${16 * sizeMultiplier}px`,
+      fontSize: `${12 * sizeMultiplier}px`,
+      '--icon-size': `${12 * sizeMultiplier}px`
+    }"
+  >
     <slot />
   </button>
 </template>
